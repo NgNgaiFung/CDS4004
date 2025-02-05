2d957dcb66f66ccd5212c0ee82faaececae88ec2
AFUNG NG <ngaifungng@ln.hk>
2025-02-05 16:46:04 +0800

    T03 changed

--- T03/script.js
+++ T03/script.js
@@ -1,7 +1,9 @@
-let display = "0";
+let ram = "0";
+let display = "0"
 let trigger = 0;
+let trigger2 = 0;
 
-// Initial display
+// Initial ram
 document.getElementById('displayValue').innerText = display;
 
 function situation_seperator(n) {
@@ -19,70 +21,92 @@ function situation_seperator(n) {
 }
 
 function addDot() {
-    if (!display.includes(".")) {
+    if (!ram.includes(".")) {
+        ram += ".";
         display += ".";
     }
     document.getElementById('displayValue').innerText = display;
 }
 
 function clear() {
+    ram = "0";
     display = "0";
-    ram = 0;
+    trigger = 0;
+    trigger2 = 0;
     document.getElementById('displayValue').innerText = display;
 }
 
 function operatorOnClick(operator) {
-    let a = document.getElementById("operators")
-    let b = a.getElementsByClassName("operator");
-    for (let i = 0; i < b.length; i++) {
-        b[i].classList.remove("active");
-    }
-    if (display === "0") return;
+    operator_status(4);
+    if (ram === "0") return;
+    if (ram.includes("+") || ram.includes("-") || ram.includes("x") || ram.includes("/"))
+        calculate();
     switch (operator) {
         case "+":
-            console.log(operators[0]);
-            b[0].classList.add("active");
-            display += " + ";
+            operator_status(0);
+            ram += " + ";
             break;
         case "-":
-            b[1].classList.add("active");
-            display += " - ";
+            operator_status(1);
+            ram += " - ";
             break;
         case "x":
-            b[2].classList.add("active");
-            display += " x ";
+            operator_status(2);
+            ram += " x ";
             break;
         case "/":
-            b[3].classList.add("active");
-            display += " / ";
+            operator_status(3);
+            ram += " / ";
             break;
     }
-    trigger = 0;
+    trigger = 2;
     document.getElementById('displayValue').innerText = display;
 }
 
 function numberOnClick(digit) {
+    if (trigger === 1) {
+        ram = "0";
+        display = "0";
+        trigger = 0;
+    }
+    if ((ram.includes("+") || ram.includes("-") || ram.includes("x") || ram.includes("/")) && trigger2 === 0) {
+        console.log(trigger2);
+        trigger2 = 1;
+        display = "0";
+    }
     if (digit === ".") return addDot();
-    if (trigger === 1) {display = ""; trigger = 0;}
-    if (digit === "0" && display === "0") return;
+    if (digit === "0" && ram === "0") return;
+    if (ram === "0") ram = "";
     if (display === "0") display = "";
+    ram += digit;
     display += digit;
     document.getElementById('displayValue').innerText = display;
 }
 
 function calculate() {
+    if (!ram.includes("+") || !ram.includes("-") || !ram.includes("x") || !ram.includes("/")) return;
     try {
-        let result = eval(display.replace(/x/g, '*'));
-        display = result.toString();
+        let result = eval(ram.replace(/x/g, '*'));
+        ram = result.toString();
+        display = ram
+        trigger = 1;
+        trigger2 = 0;
+        operator_status(4);
     } catch (e) {
-        display = "Error";
+        ram = "Error";
     }
-    trigger = 1;
     document.getElementById('displayValue').innerText = display;
 }
 
-
-
+function operator_status(idx) {
+    let a = document.getElementById("operators")
+    let b = a.getElementsByClassName("operator");
+    for (let i = 0; i < b.length; i++) {
+        b[i].classList.remove("active");
+    }
+    if (idx === 4) return;
+    b[idx].classList.add("active");
+}
 
 
 
